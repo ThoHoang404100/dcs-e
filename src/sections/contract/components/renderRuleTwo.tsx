974d7c3a1f10fa1a3b76059330d0dc169bd03d69
F88-Bao <baotq6@f88.vn>
2026-08-26 16:57:15 +0700

    Fix UI

--- src/sections/contract/components/renderRuleTwo.tsx
+++ src/sections/contract/components/renderRuleTwo.tsx
@@ -50,28 +50,14 @@ export const renderRuleTwo = ({ downPayment, nextPayment, lastPayment, signature
                 <RenderRuleName first="ĐIỀU 2:" second="GIÁ TRỊ VÀ PHƯƠNG THỨC THANH TOÁN HỢP ĐỒNG:" />
             </View>
             <RenderRuleNameChild sentence={`2.1 Tổng giá trị hợp đồng là: ${fCurrencyNoUnit(total)} VNĐ`} />
-            <View style={{
-                flexDirection: 'row',
-                gap: 10,
-                paddingRight: 50,
-            }}>
-                <Text
-                    style={{
-                        fontFamily: 'Niramit-italic',
-                        textIndent: 22,
-                        fontSize: 13
-                    }}
-                >
-                    {`(Bằng chữ:`}
-                </Text>
-                <Text
-                    style={{
-                        fontFamily: 'Niramit-BoldItalic',
-                        textIndent: 22,
-                        fontSize: 13
-                    }}
-                >
-                    {`${capitalizeFirstLetter(fRenderTextNumber(total || 0))}).`}
+            <View style={{ paddingRight: 50 }}>
+                <Text style={{ fontSize: 13, textIndent: 22, textAlign: 'justify', lineHeight: 1.6 }}>
+                    <Text style={{ fontFamily: 'Niramit-italic' }}>
+                        {`(Bằng chữ: `}
+                    </Text>
+                    <Text style={{ fontFamily: 'Niramit-BoldItalic' }}>
+                        {`${capitalizeFirstLetter(fRenderTextNumber(total || 0))}).`}
+                    </Text>
                 </Text>
             </View>
             <View style={{
